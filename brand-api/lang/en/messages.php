b1c01968058c80dfcaebfb221ec68b4ebac58b02
black-code <legrandmahamat1@gmail.com>
2025-04-17 13:23:13 +0100

    add translation files for the error message and success message i used in the brand controller

---
+++ brand-api/lang/en/messages.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'success' => [
+        'remove' => 'The :model have been remove successfully',
+    ],
+    'error' => [
+        'remove' => 'Unable to remove the :model in storage',
+        'update' => 'Unable to update the :model in storage',
+        'store' => 'Unable to save the :model in storage',
+    ],
+    'models' => [
+        'brand' => 'Brand',
+    ],
+];
---
+++ brand-api/lang/fr/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'success' => [
+        'remove' => 'Le :model a été supprimé avec succès',
+    ],
+    'error' => [
+        'remove' => 'Impossible de supprimer le :model dans le stockage',
+        'update' => 'Impossible de mettre à jour le :model dans le stockage',
+        'store' => 'Impossible d\'enregistrer le :model dans le stockage',
+       ],
+
+    'models' => [
+        'brand' => 'Brand',
+        ]
+];
